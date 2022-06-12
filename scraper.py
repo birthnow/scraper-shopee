9c5e0573c8a851f0e7105004c4d23cefa07c081e
Duncan <chienweiwhite@gmail.com>
2022-06-12 14:21:03 +0800

    5.6 批次爬取子網頁的資料

--- scraper.py
+++ scraper.py
@@ -16,11 +16,25 @@ cards = driver.find_elements(By.CSS_SELECTOR, 'div[class="col-xs-2 recommend-pro
 
 items = []
 for card in cards:
-    ActionChains(driver).move_to_element(card).perform()
+    # ActionChains(driver).move_to_element(card).perform()
 
     title = card.find_element(By.CSS_SELECTOR, 'div[class="ie3A+n bM+7UW Cve6sh"]').text
     price = card.find_element(By.CSS_SELECTOR, 'div[class="vioxXd rVLWG6"]').text
     link = card.find_element(By.TAG_NAME, 'a').get_attribute('href')
     items.append((title, price, link))
 
-print(items)
+# print(items)
+results = []
+for item in items:
+    driver.get(item[2])
+
+    for i in range(5):
+        driver.execute_script("window.scrollTo(0, document.body.scrollHeight)")
+        time.sleep(3)
+    
+    comments = driver.find_elements(By.CSS_SELECTOR, 'div[class="Em3Qhp"]')
+    for comment in comments:
+        results.append((item[0], item[1], comment.text))
+    
+    break
+print(results)
